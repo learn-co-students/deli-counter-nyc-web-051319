e968585e5a9e9cdffb9b75d293171a4f507dfbec
Elma Rahman <elmarahman635@gmail.com>
2019-04-16 03:06:57 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,32 @@
-def line
-  if #nobody is in line == "The line is currently empty."
+
+katz_deli[]
+
+def line (katz_deli)
+  katz_deli.each_with_index | current_person, index |
+  katz_deli.push("#{index}, #{current_person}")
+if
+
+
+
+
+  else
+     "The line is currently empty."
 
 end
 
 
-def take_a_number
+def take_a_number(katz_deli, index)
+
+katz_deli.each do | current_person, index |
+
+puts "Welcome, #{katz_deli}. You are number #{index+1} in line"
+end
+
+def now_serving
+  #puts next in line and removes from front of line
+
+
+else
+  puts "there is nobody wating to be served!"
 
 end
